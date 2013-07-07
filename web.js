b7a2a4482a40d6fb33a6c0217408a91aa50004c5
Zhu Xiaoen <zhuxiaoen@baidu.com>
2013-07-07 10:03:26 +0800

    Load index.html content for display.

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var content = fs.readFileSync('./index.html')
+var str = content.toString();
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(str);
 });
 
 var port = process.env.PORT || 5000;
